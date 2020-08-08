c26ee635b041bf9671fcb92a818dc01c37e510dd
tjoubert-moureaud <theo.joubert-moureaud@amadeus.com>
2020-08-08 11:13:16 +0200

    Init + babel, pwa, router, vuex, eslint, unit-mocha, e2e-cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,24 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
+    'eslint:recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        mocha: true
+      }
+    }
+  ]
 }
